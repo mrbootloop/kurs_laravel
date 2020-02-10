d994dfa931cfba0b2d3beb953882f517a0a98b03
Krzysztof Markiewicz <markiewicz.krzysztof94@gmail.com>
2020-02-10 20:55:53 +0100

    wstępna struktura formularza wersja 1

--- resources/views/videos/create.blade.php
+++ resources/views/videos/create.blade.php
@@ -1,1 +1,22 @@
-<h2>Nowy film</h2>
+@extends('master')
+@section('include')
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <div class="card">
+            <div class="panel-body">
+            <!-- Formularz -->
+            	<form action="">
+            		
+            		<div class="form-group">
+                        <label for="" class="col-md-4 control-label">Tytuł</label>
+                        <div class="col-md-6">
+                            <input type="text" class="form-control" />
+                        </div>
+                    </div>
+
+            	</form>
+            </div>
+        </div>
+    </div>
+</div>
+@stop
